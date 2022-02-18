f6760f681f93d9f45799678deb032d54196eea82
AshwaniSingh97 <99910093+AshwaniSingh97@users.noreply.github.com>
2022-02-18 12:31:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
- body {background-color:black;font-family:arial;color:white}
-h1{color:grey;font-family:verdana}
-.maindiv {
-    background-color:white;
-    color:black;
-    width:300px;
-    padding:20px;
-    border-radius:25px;
-    box-align:center
+function compute()
+{
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var amount =parseInt(principal)+parseInt(interest);
+    var year = new Date().getFullYear()+parseInt(years);
+    
+document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
 }
+function updateRate()   
+        {
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+        }
+        
